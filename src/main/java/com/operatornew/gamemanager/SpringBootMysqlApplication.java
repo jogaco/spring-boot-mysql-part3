444ce9efda12c74efed56a7e9b75a4dda1c11d15
jogaco <jogaco.en@gmail.com>
2017-05-21 18:02:45 +0200

    Initial Spring Initializr generated skeleton

---
+++ src/main/java/com/operatornew/gamemanager/SpringBootMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.operatornew.gamemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlApplication.class, args);
+	}
+}
